0b906d1c8bb358bc4f300fbdd2b7d9608a14ef65
THALYSON RAFAEL <rafinha.head@gmail.com>
2024-09-10 00:45:33 -0300

    fundamentos 001

---
+++ app/index.js
@@ -0,0 +1,11 @@
+//variavel
+let mensagem = "hello word";
+
+//escopo
+{
+    const mensagem = "não pode mudar variavel em const"
+    console.log(mensagem);
+}
+
+// hello word
+console.log(mensagem);
